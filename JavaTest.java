0442e96708f94097b98857e9c5e86d4ac73aeafe
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2024-05-24 14:01:12 +0530

    "Removed import statement, added Javadoc comment, and reformatted code in JavaTest.java"

--- JavaTest.java
+++ JavaTest.java
@@ -1,18 +1,24 @@
-import java.util.*;
+
 class addtion 
+
+
 {
-   
-    public static void main(String args[])
-{
-    int a=55,b=55,c;
-    c=a+b;
-    System.out.println("The addition of given number " + c);
-    c=a-b;
-    System.out.println("The substraction of given number is" + c);
-    c=a*b;
-    System.out.println("The multiplication of given number is" + c);
-    c=a/b;
-    System.out.println("The divison " + c);
+        /**
+         * @param args
+         */
+        public static void main(String args[])
+{   
+        int a=55,b=55,c;
+        c=a+b;
+        System.out.println("The addition of given number " + c);
+        c=a-b;
+        System.out.println("The substraction of given number is" + c);
+        c=a*b;
+        System.out.println("The multiplication of given number is" + c);
+        c=a/b;
+        System.out.println("The divison " + c);
 
 }
+
 }
+
