6f53725a5a948cb7cbc609d8eb5c5c42f7094c1f
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2024-05-27 19:04:33 +0530

    Demonstrate Relational operator

---
+++ RelationalOp.java
@@ -0,0 +1,15 @@
+public class RelationalOp
+{
+    public static void main(String[] args){
+        int a=2,b=4;
+        System.out.println("a = " + a);
+        System.out.println("b= " +b);
+        System.out.println("a<b = "+(a>b));
+        System.out.println("a>b = "+(a>b));
+        System.out.println("a<=b "+(a<=b));
+        System.out.println("a>=b "+(a>=b));
+        System.out.println("a==b "+(a==b));
+        System.out.println("a!=b "+(a!=b));
+    }
+    
+}
