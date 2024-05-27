5534a46d79481254ebfeccb285747c2061bfb907
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2024-05-27 20:21:49 +0530

    New file logicaloperator

---
+++ Logicaloperator.java
@@ -0,0 +1,21 @@
+public class Logicaloperator
+{
+    public static void main(String arg[])
+    {
+           int tm=52 ,pm=22,t;
+           t=tm+pm;
+           System.out.println("Total = "+t);
+           if(tm>=20 && pm>=15 && t==11)
+           {
+            System.out.println("Remark = Pass");
+
+           }
+           else
+           {
+            System.out.println("Remark = Fail");
+           }
+    }
+
+    
+
+}
