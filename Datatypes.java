a83f6929d5e4241a801fd60f0236f7e42fcd2986
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2024-05-27 09:29:16 +0530

    fvtdf f

---
+++ Datatypes.java
@@ -0,0 +1,15 @@
+public class Datatypes {
+    public static void main(String arg[])
+    {
+        float a=10f,b=2.5f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        
+    }
+    
+}
